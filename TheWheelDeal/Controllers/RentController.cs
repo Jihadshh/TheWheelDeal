3f07297f2012d8115a9f0bb3bbb66cf7b222df77
jihadshh <jihad.shaito@hotmail.com>
2021-12-28 18:39:26 +0200

    Add project files.

---
+++ TheWheelDeal/Controllers/RentController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TheWheelDeal.Models;
+
+namespace TheWheelDeal.Controllers
+{
+ 
+    public class RentController : Controller
+    {
+        supercarEntities db = new supercarEntities();
+        // GET: Rent
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Getcar()
+        {
+            var car = db.carregs.ToList();
+            return Json(car, JsonRequestBehavior.AllowGet);
+        }
+        [HttpPost]
+        public ActionResult Getid(int id)
+        {
+           var customer = (from s in db.customers where s.id==id select s.custname).ToList();
+            return Json(customer, JsonRequestBehavior.AllowGet);
+        }
+        [HttpPost]
+        public ActionResult Getavil(String carno)
+        {
+            var caravil = (from s in db.carregs where s.carno == carno select s.available).FirstOrDefault();
+            return Json(caravil, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
